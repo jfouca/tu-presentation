1d8ee6fdd0c594f483334028d9b2f6046bbec3b7
jfouca <foucault.jeremy@gmail.com>
2014-11-06 00:21:32 +0100

    [master] Upgrades homepage

--- app/modules/translator/translator.js
+++ app/modules/translator/translator.js
@@ -5,6 +5,7 @@ var translator = angular.module('translator', ['pascalprecht.translate']);
 translator.config(function($translateProvider) {
     $translateProvider.translations('en', {
         brand: 'Teams\'Up',
+        brand_site: 'Teams\'Up.net',
         // header
         header_home_title: 'Home',
         header_contact_title: 'Contact',
@@ -27,6 +28,17 @@ translator.config(function($translateProvider) {
         hello: 'Hello World !',
         home_body: 'You want to play some board games, but you have no fellows ? Time to find some !',
         home_learnmore: 'Learn more',
+        home_meet_near_you: 'Meet new players close to you',
+        first_explanation_title: 'PLAY a card game, board game, role player game...',
+        home_play_with_whom: 'Ok but still, with whom ? Where ? Setting up a party can be painful.',
+        home_our_solution: 'is our solution! Find what you need easily (friends, game master, a place to sit etc...) near you.',
+        home_free_solution: 'It is new, it is free and it is for you guys !',
+        home_still_dev: 'The website is still under development, if you want to learn more about us, please click  ',
+        second_explanation_title: 'Interested by our project ?',
+        home_please_register: 'Sign up now, we will keep you in touch with our progress and show us your interest.',
+        home_spread_the_word: 'Talk to this website to your friends.',
+        home_signeup: 'Sign up for free',
+        home_no_creditcard: 'No credit card needed',
 
         // about
         about_title: 'is a plateform where you can find some other players near you that want to play some board games as well !',
@@ -46,6 +58,7 @@ translator.config(function($translateProvider) {
     });
     $translateProvider.translations('fr', {
         brand: 'Teams\'Up',
+        brand_site: 'Teams\'Up.net',
         // header
         header_home_title: 'Home',
         header_contact_title: 'Nous contacter',
@@ -68,6 +81,18 @@ translator.config(function($translateProvider) {
         hello: 'Hello World !',
         home_body: 'Vous désirez jouer à des jeux de plateau, de cartes, de rôle, mais vous ne connaissez personne ? Le moment est venu pour vous d\'y remédier',
         home_learnmore: 'En apprendre d\'avantage',
+        home_meet_near_you: 'Rencontrez des joueurs près de chez vous',
+        first_explanation_title: 'JOUER à un jeu de cartes, de plateau, de rôle, de figurines...',
+        home_play_with_whom: 'OUI ! Mais avec qui ? Et où ? Organiser une après-midi ou une soirée est souvent problématique.',
+        home_our_solution: 'c\’est notre solution ! Exprimer votre besoin et trouvez facilement ce qu\’il vous manque (des amis, un Maître du jeu, un endroit où vous retrouver ...) près de chez vous !',
+        home_free_solution: 'C\’est nouveau, c\’est gratuit, et c\’est pour vous.',
+        home_still_dev: 'Ce site est toujours en cours de développement, si vous voulez en savoir PLUS sur le projet ou sur nous, cliquez ',
+        second_explanation_title: 'Notre projet vous intéresse ?',
+        home_please_register: 'Inscrivez-vous dès maintenant pour etre informés dès qu\'une première version sera en ligne. Et montrez-nous ainsi votre soutien =).',
+        home_spread_the_word: 'N\'hésitez pas à en parler autour de vous.',
+        home_signeup: 'Inscrivez-vous gratuitement',
+        home_no_creditcard: 'Aucune carte de crédit requise.',
+
 
         // about
         about_title: 'est une plateforme où vous pouvez facilement trouver d\'autres joueurs près de chez vous qui désirent également se lancer dans un jeu.',
